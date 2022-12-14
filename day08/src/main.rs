77231f8c4331958a036e135b09221e8ab716ea4e
Scott Haiden <scott.b.haiden@gmail.com>
2022-12-13 23:04:16 -0800

    Not pretty but it solves part 2.

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -43,10 +43,50 @@ impl Map {
         }
     }
 
-    fn get_column(&mut self, col: usize) -> Vec<&mut Tree> {
+    fn get_column_mut(&mut self, col: usize) -> Vec<&mut Tree> {
         return self.trees.iter_mut()
             .map(|row| &mut row[col])
             .collect();
+    }
+
+    fn get_column(&self, col: usize) -> Vec<Tree> {
+        return self.trees.iter()
+            .map(|row| row[col])
+            .collect();
+    }
+
+    fn get_score(&self, coords: (usize, usize)) -> usize {
+        fn get_score(trees: &mut dyn Iterator<Item=&Tree>) -> usize {
+            let tree = trees.next().unwrap();
+
+            let mut len = 0;
+            let mut tallest = -1i32;
+
+            for (i, cur) in trees.enumerate() {
+                len = i;
+                tallest = cur.height as i32;
+
+                if tallest >= tree.height as i32 { break; }
+            }
+
+            if tallest < 0i32 { return 0; }
+            return len + 1;
+        }
+
+        let (row, col) = coords;
+
+        let row_trees = &self.trees[row];
+        let from_right = get_score(&mut row_trees[..=col].iter().rev());
+        let from_left = get_score(&mut row_trees[col..].iter());
+
+        let col_trees = self.get_column(col);
+        let from_bottom = get_score(&mut col_trees[..=row].iter().rev());
+        let from_top = get_score(&mut col_trees[row..].iter());
+
+        let ret = from_right * from_left * from_bottom * from_top;
+        println!("{:?}: up {} down {} left {} right {} total {}",
+                 coords, from_bottom, from_top, from_right, from_left, ret);
+        return ret;
     }
 
     fn update_visibilities(&mut self) {
@@ -65,10 +105,22 @@ impl Map {
             mark_visibles(&mut row.iter_mut().rev());
         }
         for col in 0..self.trees.len() {
-            mark_visibles(&mut self.get_column(col).into_iter());
-            mark_visibles(&mut self.get_column(col).into_iter().rev());
+            mark_visibles(&mut self.get_column_mut(col).into_iter());
+            mark_visibles(&mut self.get_column_mut(col).into_iter().rev());
         }
     }
+}
+
+fn cartesian_product(len: usize) -> Vec<(usize, usize)> {
+    let mut ret = Vec::new();
+
+    for i in 0..len {
+        for j in 0..len {
+            ret.push((i, j));
+        }
+    }
+
+    return ret;
 }
 
 fn main() {
@@ -91,6 +143,13 @@ fn main() {
         .filter(|i| i.visible)
         .count();
 
+    let best_spot = cartesian_product(map.trees.len())
+        .iter()
+        .map(|coords| map.get_score(*coords))
+        .max()
+        .unwrap();
+
     map.print_map();
     println!("there are {} visible trees.", visible_trees);
+    println!("the best spot has a score of {}.", best_spot);
 }
